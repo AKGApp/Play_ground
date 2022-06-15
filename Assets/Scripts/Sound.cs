d4003ad386ef5f45142aa2c1d44ac12520db609e
7arakat <ziadharakat44@gmail.com>
2022-06-15 16:00:24 +0300

    Sounds
    Added the sounds to the buttons, Slight problem: Sound wouldn't play as the program is switching to the next scene

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -5,17 +5,6 @@ using UnityEngine;
 public class Sound : MonoBehaviour
 {
     public AudioSource SoundPlayer;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void PlaySound()
     {
         SoundPlayer.Play();
